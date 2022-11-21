26facf0f601ad76325b57f60e3bb2039a794ba73
Thiago <thidurante@hotmail.com>
2022-11-21 10:03:57 -0300

    Adds nav and Header

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <section>
+      <nav>Search</nav>
+      <nav>Tech used</nav>
+      <nav>Who is the dev?</nav>
+    </section>
+  );
+};
+
+export default Nav;
